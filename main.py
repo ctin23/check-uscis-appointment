798e9934c7d4b1b1f88cdf0b63aa94f9904fdf5f
Chak Hei Tin <ctin23@berkeley.edu>
2023-05-09 22:43:45 -0700

    write log

--- main.py
+++ main.py
@@ -39,21 +39,26 @@ def send_email():
         for receiver_email in receiver_emails:
             server.sendmail(sender_email, receiver_email, message)
 
+def write_log(data):
+    f = open("log.txt", "a")
+    log = "Time: {time}, Data: {data}\n".format(time = datetime.datetime.now(), data = data)
+    f.write(log)
+    f.close()
+
 def main():
-    minute = 5
+    log_count = 0
     while True:
         available, data = check_availability()
         if available:
-            print("Got em! Sending email now... at %s", datetime.datetime.now())
+            print("Got em! Sending email now... at ", datetime.datetime.now())
             send_email()
             break
         else:
             print("Fuck! Sleeping", rate, "seconds and try again: ", datetime.datetime.now())
-            f = open("log.txt", "a")
-            log = "Time: {time}, Data: {data}\n".format(time = datetime.datetime.now(), data = data)
-            f.write(log)
-            f.close()
+            if log_count % 10 == 0:
+                write_log(data)
             time.sleep(rate)
+        log_count += 1
 
 
 if __name__ =="__main__":
